7fc737ee158f0d24d953bf34ae66732417f488fa
Paulo Caselli <paulov_caselli@live.com>
2019-09-19 20:53:26 -0300

    Add arquivos

---
+++ Aulas Prog2/src/TesteArray.java
@@ -0,0 +1,12 @@
+
+public class TesteArray {
+
+	public static void main(String[] args) {
+		float cotacoes[] = {1.74f, 1.55f, 1.81f, 1.27f}; 
+		for (float cot: cotacoes) {
+			System.out.println(cot);
+		}
+		
+	}
+
+}
